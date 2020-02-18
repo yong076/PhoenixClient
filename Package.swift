8326b6745b07ae458b8cf9141e5786310ca057f0
daniel.dev <daniel.dev@kakaoenterprise.com>
2020-02-18 18:49:13 +0900

    init

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:4.2
+
+import PackageDescription
+
+let package = Package(
+  name: "PhoenixClient",
+  products: [
+    .library(name: "PhoenixClient", targets: ["PhoenixClient"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.1.1"),
+  ],
+  targets: [
+    .target(
+      name: "PhoenixClient",
+      dependencies: ["Starscream"]
+    ),
+  ]
+)
---
+++ PhoenixClient/PhoenixClient.h
@@ -0,0 +1,19 @@
+//
+//  PhoenixClient.h
+//  PhoenixClient
+//
+//  Created by wade.hawk on 2019/11/10.
+//  Copyright © 2019 wade.hawk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PhoenixClient.
+FOUNDATION_EXPORT double PhoenixClientVersionNumber;
+
+//! Project version string for PhoenixClient.
+FOUNDATION_EXPORT const unsigned char PhoenixClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhoenixClient/PublicHeader.h>
+
+
